2cf878f4a677a72151e6492a2cb11ad5d386c5ce
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2010-04-23 14:31:51 +0000

    Adding survey.py and first_baby.py

---
+++ code/survey.py
@@ -0,0 +1,116 @@
+"""This file contains code for use with "Think Stats",
+by Allen B. Downey, available from greenteapress.com
+
+Copyright 2010 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+"""
+
+import sys
+import gzip
+
+class Record(object):
+    """Represents a record."""
+
+class Respondent(Record): 
+    """Represents a respondent."""
+
+class Pregnancy(Record):
+    """Represents a pregnancy."""
+
+class Table(object):
+    """Represents a table as a list of objects"""
+
+    def ReadFile(self, filename, fields, constructor):
+        """Reads a compressed data file builds one object per record.
+
+        Args:
+            filename: string name of the file to read
+
+            fields: sequence of (name, start, end, cast) tuples specifying 
+            the fields to extract
+
+            constructor: what kind of object to create
+        """
+        if filename.endswith('gz'):
+            fp = gzip.open(filename)
+        else:
+            fp = open(filename)
+
+        for line in fp:
+            record = self.MakeRecord(line, fields, constructor)
+            self.AddRecord(record)
+        fp.close()
+    
+    def MakeRecord(self, line, fields, constructor):
+        """Scans a line and returns an object with the appropriate fields.
+
+        Args:
+            line: string line from a data file
+
+            fields: sequence of (name, start, end, cast) tuples specifying 
+            the fields to extract
+
+            constructor: callable that makes an object for the record.
+
+        Returns:
+            Record with appropriate fields.
+        """
+        obj = constructor()
+        for (field, start, end, cast) in fields:
+            try:
+                val = cast(line[start-1:end])
+            except ValueError:
+                val = 'NA'
+            setattr(obj, field, val)
+        return obj
+
+    def AddRecord(self, record):
+        """Adds a record to this table.
+
+        Args:
+            record: an object of one of the record types.
+        """
+        self.records.append(record)
+
+class Respondents(Table):
+    """Represents the respondent table."""
+
+    def __init__(self, filename='2002FemResp.dat.gz'):
+        self.records = []
+        self.ReadFile(filename, self.GetFields(), Respondent)
+
+    def GetFields(self):
+        """Returns a tuple specifying the fields to extract.
+
+        The elements of the tuple are field, start, end, case.
+
+                field is the name of the variable
+                start and end are the indices as specified in the NSFG docs
+                cast is a callable that converts the result to int, float, etc.
+        """
+        return [
+            ('caseid', 1, 12, int),
+            ]
+
+class Pregnancies(Table):
+    def __init__(self, filename='2002FemPreg.dat.gz'):
+        self.records = []
+        self.ReadFile(filename, self.GetFields(), Pregnancy)
+
+    def GetFields(self):
+        return [
+            ('caseid', 1, 12, int),
+            ('prglength', 275, 276, int),
+            ('outcome', 277, 277, int),
+            ('birthord', 278, 279, int),
+            ('finalwgt', 423, 440, float),
+            ]
+
+def main(name):
+    resp = Respondents()
+    print 'Number of respondents', len(resp.records)
+    preg = Pregnancies()
+    print 'Number of pregnancies', len(preg.records)
+    
+if __name__ == '__main__':
+    main(*sys.argv)
