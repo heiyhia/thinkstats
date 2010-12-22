23fc9f8bf19165c59e9a93eaf4f76b24ecba49c0
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2010-12-22 21:46:45 +0000

    Updating Pmf.py and Cdf.py

--- workspace.thinkstats/ThinkStats/Cdf_test.py
+++ workspace.thinkstats/ThinkStats/Cdf_test.py
@@ -10,18 +10,20 @@ import Cdf
 
 class Test(unittest.TestCase):
 
-    def setUp(self):
+    def testMakeCdfFromList(self):
         t = [2, 1, 3, 2, 5]
-        self.cdf = Cdf.MakeCdfFromList(t, 'bob')
+        cdf = Cdf.MakeCdfFromList(t, 'bob')
+        self.checkCdf(cdf)
 
-    def testMakeCdf(self):
-        cdf = self.cdf
+    def checkCdf(self, cdf):
         self.assertEqual(cdf.xs, [1, 2, 3, 5])
         self.assertEqual(cdf.ps, [0.2, 0.6, 0.8, 1.0])
         self.assertEqual(cdf.name, 'bob')
 
     def testProb(self):
-        cdf = self.cdf
+        t = [2, 1, 3, 2, 5]
+        cdf = Cdf.MakeCdfFromList(t, 'bob')
+
         self.assertEqual(cdf.Prob(-1), 0.0)
         self.assertEqual(cdf.Prob(1), 0.2)
         self.assertEqual(cdf.Prob(2), 0.6)
@@ -31,7 +33,9 @@ class Test(unittest.TestCase):
         self.assertEqual(cdf.Prob(7), 1.0)
 
     def testValue(self):
-        cdf = self.cdf
+        t = [2, 1, 3, 2, 5]
+        cdf = Cdf.MakeCdfFromList(t, 'bob')
+
         self.assertEqual(cdf.Value(0.0), 1)
         self.assertEqual(cdf.Value(0.1), 1)
         self.assertEqual(cdf.Value(0.2), 1)
@@ -47,19 +51,24 @@ class Test(unittest.TestCase):
         self.assertRaises(ValueError, cdf.Value, 1.1)
 
     def testMean(self):
-        cdf = self.cdf
+        t = [2, 1, 3, 2, 5]
+        cdf = Cdf.MakeCdfFromList(t, 'bob')
         self.assertAlmostEqual(cdf.Mean(), 13.0/5.0)
 
-    def testGetItems(self):
-        cdf = self.cdf
-        items = cdf.GetItems()
+    def testItems(self):
+        t = [2, 1, 3, 2, 5]
+        cdf = Cdf.MakeCdfFromList(t, 'bob')
+
+        items = cdf.Items()
         expected = [(1, 0.2), (2, 0.6), (3, 0.8), (5, 1.0)]
         for p1, p2 in zip(items, expected):
             for x1, x2 in zip(p1, p2):
                 self.assertEqual(x1, x2)
         
     def testRender(self):
-        cdf = self.cdf
+        t = [2, 1, 3, 2, 5]
+        cdf = Cdf.MakeCdfFromList(t, 'bob')
+
         vs, ps = cdf.Render()
         self.assertEqual(vs, [1, 1, 2, 2, 3, 3, 5, 5])
         for got, expected in zip(ps, [0.0, 0.2, 0.2, 0.6, 0.6, 0.8, 0.8, 1.0]):
--- workspace.thinkstats/ThinkStats/Pmf.py
+++ workspace.thinkstats/ThinkStats/Pmf.py
@@ -7,7 +7,9 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
 """Functions for building PMFs (probability mass functions)."""
 
-class Hist(object):
+class _DictWrapper(object):
+    """An object that contains a dictionary."""
+
     def __init__(self, d=None, name=''):
         if d == None:
             d = {}
@@ -15,8 +17,32 @@ class Hist(object):
         self.name = name
 
     def GetDict(self):
+        """Gets the dictionary."""
         return self.d
 
+    def Values(self):
+        """Gets an iterator of the values.
+
+        Note: one source of confusion is that the keys in this
+        dictionaries are the values of the Hist/Pmf, and the
+        values are frequencies/probabilities.
+        """
+        return self.d.iterkeys()
+
+    def Items(self):
+        """Gets an iterator of the items (value, freq/prob) pairs."""
+        return self.d.iteritems()
+
+    def Render(self):
+        """Generates a sequence of points suitable for plotting.
+
+        Returns:
+            tuple of (sorted value sequence, freq/prob sequence)
+        """
+        return zip(*sorted(self.Items()))
+
+
+class Hist(_DictWrapper):
     def Incr(self, x, count=1):
         """Increments the counter associated with the value x.
 
@@ -25,10 +51,6 @@ class Hist(object):
             count: how much to increment by
         """
         self.d[x] = self.d.get(x, 0) + count
-
-    def Values(self):
-        """Gets a sorted iterator of the values."""
-        return sorted(self.d.iteritems())
 
     def Freq(self, x):
         """Gets the frequency associated with the value x.
@@ -52,21 +74,12 @@ class Hist(object):
         return zip(*items)
     
 
-class Pmf(object):
+class Pmf(_DictWrapper):
     """Represents a probability mass function.
 
     Attributes:
         map: dictionary that maps from values to probabilities
     """
-    def __init__(self, d=None, name=''):
-        if d == None:
-            d = {}
-        self.d = d
-        self.name = name
-        
-    def GetDict(self):
-        return self.d
-
     def Incr(self, x, p=1):
         """Increments the probability associated with the value x.
 
@@ -84,21 +97,6 @@ class Pmf(object):
             factor: how much to multiply by
         """
         self.d[x] = self.d.get(x, 0) * factor
-
-    def Values(self):
-        """Gets a sorted iterator of the values.
-
-        Note one source of confusion: the values in this PMF are stored
-        as the _keys_ of the dictionary.  The probabilities are stored
-        as values in the dictionary.
-        """
-        return sorted(self.d.iterkeys())
-
-    def Items(self):
-        """Gets an unsorted iterator of the (val, prob) pairs.
-
-        """
-        return self.d.iteritems()
 
     def Prob(self, x):
         """Gets the probability associated with the value x.
@@ -157,23 +155,12 @@ class Pmf(object):
             var += p * (x - mu)**2
         return var
 
-    def Render(self):
-        """Generates a sequence of points suitable for plotting.
 
-        Returns:
-            sequence of (value, prob) pairs.
-        """
-        items = self.d.items()
-        items.sort()
-        return zip(*items)
-
-
-def MakeHist(t, name=''):
+def MakeHistFromList(t, name=''):
     """Makes a histogram from an unsorted sequence of values.
 
     Args:
         t: sequence of numbers
-
         name: string name for this histogram
 
     Returns:
@@ -184,30 +171,83 @@ def MakeHist(t, name=''):
     return hist
 
 
+def MakeHistFromDict(d, name=''):
+    """Makes a histogram from a map from values to frequencies.
+
+    Args:
+        d: dictionary that maps values to frequencies
+        name: string name for this histogram
+
+    Returns:
+        Hist object
+    """
+    return Hist(d, name)
+
+
 def MakePmfFromList(t, name=''):
     """Makes a PMF from an unsorted sequence of values.
 
     Args:
         t: sequence of numbers
-
         name: string name for this PMF
 
     Returns:
         Pmf object
     """
-    hist = MakeHist(t, name)
+    hist = MakeHistFromList(t, name)
     return MakePmfFromHist(hist)
 
-def MakePmfFromHist(hist):
-    """Makes a PMF from a Hist object.
+
+def MakePmfFromDict(d, name=''):
+    """Makes a PMF from a map from values to probabilities.
 
     Args:
-        hist: Hist object
+        d: dictionary that maps values to probabilities
+        name: string name for this PMF
 
     Returns:
         Pmf object
     """
+    return Pmf(d, name)
+
+
+def MakePmfFromHist(hist, name=None):
+    """Makes a PMF from a Hist object.
+
+    Args:
+        hist: Hist object
+        name: string name
+
+    Returns:
+        Pmf object
+    """
+    if name is None:
+        name = hist.name
+
+    # make a copy of the dictionary
     d = dict(hist.GetDict())
     pmf = Pmf(d, hist.name)
     pmf.Normalize()
+    return pmf
+
+
+def MakePmfFromCdf(cdf, name=None):
+    """Makes a PMF from a Cdf object.
+
+    Args:
+        cdf: Cdf object
+
+    Returns:
+        Pmf object
+    """
+    if name is None:
+        name = cdf.name
+
+    pmf = Pmf()
+
+    prev = 0.0
+    for val, prob in cdf.Items():
+        pmf.Incr(val, prov-prev)
+        prev = prob
+
     return pmf
