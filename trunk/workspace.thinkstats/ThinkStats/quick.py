bec1365042baf5ebb45edc2aadbfc2c0c4b69c90
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2011-05-17 16:24:59 +0000

    Updating quick.py

--- trunk/workspace.thinkstats/ThinkStats/quick.py
+++ trunk/workspace.thinkstats/ThinkStats/quick.py
@@ -1,21 +1,22 @@
 import random
 
-def Sample(n=2500):
+def Sample(n):
     return [random.random() for i in xrange(n)]
 
-def Count(sample, p):
-    return len([x for x in sample if x<p])
+def Fraction(sample, p):
+    success = len([x for x in sample if x<p])
+    return float(success) / len(sample)
 
-def Test(n=1250):
+def Test(n, p):
     s1 = Sample(n)
-    f1 = 1.0 * Count(s1, 0.07) / n
+    f1 = Fraction(s1, p)
     s2 = Sample(n)
-    f2 = 1.0 * Count(s2, 0.07) / n
+    f2 = Fraction(s2, p)
     return f1 - f2
     
 count = 0
 for i in range(10000):
-    diff = Test()
+    diff = Test(1250, 0.07)
     if diff > 0.033:
         count += 1
 
