152d8d218a1b34f381262ece65199479741db9f9
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2011-05-27 19:00:33 +0000

    Updating rarefaction

--- trunk/workspace.thinkstats/ThinkStats/rarefaction.py
+++ trunk/workspace.thinkstats/ThinkStats/rarefaction.py
@@ -7,6 +7,7 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
 import bayes
 import copy
+import string
 import matplotlib.pyplot as pyplot
 import myplot
 import Pmf
@@ -18,59 +19,135 @@ import random
 """
 
 
-def MakeUniformSuite(low, high, steps, name=None):
-    """Makes a PMF that represents a suite of hypotheses with equal p.
+class Beta(object):
+    def __init__(self, yes, no):
+        self.alpha = yes+1
+        self.beta = no+1
+
+    def Update(self, yes, no):
+        self.alpha += yes
+        self.beta += no
+        
+    def Mean(self):
+        """Computes the mean of this distribution."""
+        return float(self.alpha) / (self.alpha + self.beta)
+
+    def Random(self):
+        """Generate a random variate from this distribution."""
+        # handle the special case where there is one taxon, so
+        # its prevalence is necessarily 1
+        if self.beta == 0:
+            return 1
+
+        return random.betavariate(self.alpha, self.beta)
+
+
+class Census(object):
+    """Represents a belief about the population of a sample.
+
+    n is the hypothetical number of taxa
+
+    multiplier is the number of taxa that have not been observed.
+
+    suites is a mapping from taxon to a suite of hypotheses
+    about p, the fractional population of the taxon.
     
-    Args:
-        low: low end of range
-        high: high end of range
-        steps: number of values
+    suites['other'] is a special entry that represents the prevalence
+    of unobserved taxa.
 
-    Returns:
-        Pmf object
+    When all taxa are accounted for, suites['other'] is removed.
     """
-    hypos = [low + (high-low) * i / (steps-1.0) for i in range(steps)]
-    pmf = Pmf.MakePmfFromList(hypos, name=name)
-    return pmf
 
+    def __init__(self, n):
+        self.n = n
+        self.multiplier = n
+        self.suites = {}
+        self.suites['other'] = Beta(yes=0, no=n-2)
 
-def MakeBinomialSuite(yes, no, steps=11):
-    """Makes a PMF that represents a suite of hypotheses with equal p.
-    
-    Args:
-        low: low end of range
-        high: high end of range
-        steps: number of values
+    def Get(self, taxon):
+        """Looks up the suite for a given taxon (or None)."""
+        return self.suites.get(taxon)
 
-    Returns:
-        Pmf object
-    """
-    if no == -1:
-        return Pmf.MakePmfFromList([1])
+    def Other(self):
+        """Returns the suite for 'other', or None."""
+        return self.suites.get('other')
 
-    ps = [i / (steps-1.0) for i in range(steps)]
-    probs = [math.pow(p, yes) * math.pow(1-p, no) for p in ps]
-    pmf = Pmf.MakePmfFromDict(dict(zip(ps, probs)))
-    pmf.Normalize()
-    return pmf
+    def GetMean(self, taxon):
+        """Computes the mean value of p for a given taxon, or 0."""
+        suite = self.Get(taxon)
+        return suite.Mean() if suite else 0
 
+    def Likelihood(self, evidence):
+        """Computes the likelihood of seeing a given taxon."""
+        taxon = evidence
+        if taxon in self.suites:
+            return self.GetMean(taxon)
+        else:
+            # if we haven't see this taxon before, we have to consider
+            # multiple copies of the 'other' suite
+            return self.multiplier * self.GetMean('other')
 
-def MakeExponentialSuite(low, high, steps, lam=10.0):
-    """Makes a PMF that represents a suite of hypotheses with equal p.
-    
-    Args:
-        low: low end of range
-        high: high end of range
-        steps: number of values
+    def AddTaxon(self, taxon):
+        """The first time we see a new taxon, add it to the census."""
+        
+        # make a copy of the 'other' suite
+        suite = copy.deepcopy(self.Other())
+        if suite is None:
+            raise Exception('All taxa are accounted for.')
+        self.suites[taxon] = suite 
 
-    Returns:
-        Pmf object
-    """
-    ps = [low + (high-low) * i / (steps-1.0) for i in range(steps)]
-    probs = [math.exp(-lam*p) for p in ps]
-    pmf = Pmf.MakePmfFromDict(dict(zip(ps, probs)))
-    pmf.Normalize()
-    return pmf
+        # reduce the multiplier for the 'other' suite
+        self.multiplier -= 1
+
+        # if we just accounted for the last unseen taxon, delete 'other'
+        if self.multiplier == 0:
+            del self.suites['other']
+
+    def GeneratePrevalence(self):
+        """Generates a possible census.
+
+        Result is a PMF that maps from taxon to prevalence.
+        """
+        pmf = Pmf.Pmf(name='sample census')
+        for taxon, suite in self.suites.iteritems():
+            prob = suite.Random()
+            if taxon == 'other':
+                prob *= self.multiplier
+            pmf.Incr(taxon, prob)
+        pmf.Normalize()
+        return pmf
+
+    def GenerateTaxon(self):
+        """Choose a random taxon from this census."""
+        pmf = self.GeneratePrevalence()
+        return pmf.Random()
+
+    def Update(self, evidence):
+        """Updates each suite with new evidence.
+
+        evidence is a taxon
+        """
+        seen_taxon = evidence
+
+        # if we haven't seen this taxon before, add it
+        if seen_taxon not in self.suites:
+            self.AddTaxon(seen_taxon)
+
+        for taxon, suite in self.suites.iteritems():
+            yesno = [1, 0] if taxon==seen_taxon else [0, 1]
+            suite.Update(*yesno)
+
+    def Plot(self):
+        """
+        """
+        for taxon, suite in sorted(self.suites.iteritems()):
+            print taxon, bayes.CredibleInterval(suite, 90)
+
+    def PlotPmfs(self):
+        """Plots the PMFs for each suite."""
+        # TODO: add labels
+        pmfs = self.suites.values()
+        PlotPmfs(pmfs)
 
 
 class MetaHypo(object):
@@ -117,152 +194,44 @@ class MetaHypo(object):
         for hypo, prob in sorted(self.hypos.Items()):
             print hypo.n, prob
 
-
-class Census(object):
-    """Represents a belief about the population of a sample.
-
-    n is the hypothetical number of taxa
-
-    multiplier is the number of taxa that have not been observed.
-
-    suites is a mapping from taxon to a suite of hypotheses
-    about p, the fractional population of the taxon.
-    
-    suites['other'] is a special entry that represents the prevalence
-    of unobserved taxa.
-
-    When all taxa are accounted for, suites['other'] is removed.
-    """
-
-    def __init__(self, n):
-        self.updater = bayes.Binomial()
-        self.n = n
-        self.multiplier = n
-        self.suites = {}
-        self.suites['other'] = MakeBinomialSuite(yes=0, no=n-2, steps=101)
-
-    def Get(self, taxon):
-        """Looks up the suite for a given taxon (or None)."""
-        return self.suites.get(taxon)
-
-    def Other(self):
-        """Returns the suite for 'other', or None."""
-        return self.suites.get('other')
-
-    def GetMean(self, taxon):
-        """Computes the mean value of p for a given taxon, or 0."""
-        suite = self.Get(taxon)
-        return suite.Mean() if suite else 0
-
-    def Likelihood(self, evidence):
-        """Computes the likelihood of seeing a given taxon."""
-        taxon = evidence
-        if taxon in self.suites:
-            return self.GetMean(taxon)
-        else:
-            # if we haven't see this taxon before, we have to consider
-            # multiple copies of the 'other' suite
-            return self.multiplier * self.GetMean('other')
-
-    def AddTaxon(self, taxon):
-        """The first time we see a new taxon, add it to the census."""
-        
-        # make a copy of the 'other' suite
-        suite = copy.deepcopy(self.Other())
-        if suite is None:
-            raise Exception('All taxa are accounted for.')
-        self.suites[taxon] = suite 
-
-        # reduce the multiplier for the 'other' suite
-        self.multiplier -= 1
-
-        # if we just accounted for the last unseen taxon, delete 'other'
-        if self.multiplier == 0:
-            del self.suites['other']
-
-    def MakeCdfs(self):
-        """Generates a CDF for each suite.
-
-        Makes Generate() faster.
-        """
-        self.cdfs = {}
-        for taxon, suite in self.suites.iteritems():
-            self.cdfs[taxon] = Cdf.MakeCdfFromPmf(suite)
-
-    def Generate(self):
-        """Given the current CDFs, generate a possible census.
-
-        Result is a PMF that maps from taxon to prevalence.
-        """
-        pmf = Pmf.Pmf(name='sample census')
-        # TODO: go in descending order by mean
-        for taxon, cdf in self.cdfs.iteritems():
-            prob = cdf.Random()
-            if taxon == 'other':
-                prob *= self.multiplier
-            pmf.Incr(taxon, prob)
-        pmf.Normalize()
-        return pmf
-
     def GenerateTaxon(self):
-        """Choose a random taxon from this census."""
-        pmf = self.Generate()
-        cdf = Cdf.MakeCdfFromPmf(pmf)
-        return cdf.Random()
+        """Chooses a random taxon."""
+        pmf = Pmf.Pmf()
+        for hypo, prob in sorted(self.hypos.Items()):
+            taxon = hypo.GenerateTaxon()
+            pmf.Incr(taxon, prob)
+        return pmf.Random()
 
-    def Update(self, evidence):
-        """Updates each suite with new evidence.
+    def GenerateRarefactionCurve(self, sample, m):
+        """Generates a possible rarefaction curve.
 
-        evidence is a taxon
+        Starts with the given sample and generates m additional taxa.
+
+        Returns the complete sample and the rarefaction curve.
         """
-        seen_taxon = evidence
+        def NextNameGenerator(skip):
+            """Generates names for unobserved taxa."""
+            for name in string.ascii_lowercase[skip:]:
+                yield name
 
-        # if we haven't seen this taxon before, add it
-        if seen_taxon not in self.suites:
-            self.AddTaxon(seen_taxon)
+        names = NextNameGenerator(skip=len(set(sample)))
 
-        for taxon, suite in self.suites.iteritems():
-            yesno = [1, 0] if taxon==seen_taxon else [0, 1]
-            self.updater.Update(suite, yesno)
+        meta = copy.deepcopy(self)
+        taxons = list(sample)
+        random.shuffle(taxons)
 
-    def Plot(self):
-        """
-        """
-        for taxon, suite in sorted(self.suites.iteritems()):
-            print taxon, bayes.CredibleInterval(suite, 90)
+        for i in range(m):
+            taxon = self.GenerateTaxon()
+            if taxon == 'other':
+                taxon = names.next()
+            taxons.append(taxon)
+            meta.Update(taxon)
 
-    def PlotPmfs(self):
-        """Plots the PMFs for each suite."""
-        # TODO: add labels
-        pmfs = self.suites.values()
-        PlotPmfs(pmfs)
+        curve = MakeCurve(taxons)
+        return ''.join(taxons), curve
 
-    def GenerateRarefactionCurves(self, n, m, s, iters=50):
-        self.MakeCdfs()
-        curves = [self.GenerateCurve(n, m, s) for i in range(iters)]
-        return curves
 
-    def GenerateCurve(self, n, m, s):
-        pmf = self.Generate()
-        cdf = Cdf.MakeCdfFromPmf(pmf)
-        sample = cdf.Sample(m)
-        return self.MakeCurve(sample, n, s)
 
-    def PlotBeforeAndAfter(self, sample):
-        curves1 = self.MakePossibleCurves(sample)
-        
-        n = len(sample)
-        s = set(sample)
-        curves2 = self.GenerateRarefactionCurves(n, 15, s)
-
-        pyplot.clf()
-        PlotCurves(curves1, 'b')
-        PlotCurves(curves2, 'g')
-        myplot.Plot(show=True,
-                    title='Rarefaction curve',
-                    xlabel='# samples',
-                    ylabel='# taxa',
-                    )
 
 
 def PlotPmfs(pmfs):
@@ -272,17 +241,8 @@ def PlotPmfs(pmfs):
                 )
 
 
-def MakePossibleCurves(sample, iters=5):
-    t = list(sample)
-    curves = []
-    for i in range(iters):
-        random.shuffle(t)
-        curve = MakeCurve(t)
-        curves.append(curve)
-    return curves
-
-
 def MakeCurve(sample):
+    """Makes a rarefaction curve for the given sample."""
     s = set()
     curve = []
     for i, taxon in enumerate(sample):
@@ -292,52 +252,52 @@ def MakeCurve(sample):
 
 
 def PlotCurves(curves, color='b'):
+    """Plots a set of curves.
+
+    curves is a list of curves; each curve is a list of (x, y) pairs.
+    """
     for curve in curves:
         curve = JitterCurve(curve)
         xs, ys = zip(*curve)
         pyplot.plot(xs, ys, color)
 
-
-def JitterCurve(curve, dx=0.2, dy=0.3):
-    curve = [(x+random.uniform(-dx, dx), 
-              y+random.uniform(-dy, dy)) for x, y in curve]
-    return curve
-
-
-def main():
-    sample = 'aaabbc'
-    curves = MakePossibleCurves(sample)
-    PlotCurves(curves)
     myplot.Plot(show=True,
                 title='Rarefaction curve',
                 xlabel='# samples',
                 ylabel='# taxa',
                 )
 
-    prior = Pmf.MakePmfFromList(range(1,20))
+
+def JitterCurve(curve, dx=0.2, dy=0.3):
+    """Adds random noise to the pairs in a curve.
+
+    dx and dy control the amplitude of the noise in each dimension.
+    """
+    curve = [(x+random.uniform(-dx, dx), 
+              y+random.uniform(-dy, dy)) for x, y in curve]
+    return curve
+
+
+def main():
+    sample = 'aaaaaaaaabbbbb'
+    sample = 'aaaaaabbbbcdef'
+    sample = 'a'
+
+    prior = Pmf.MakePmfFromList(range(1, 21))
     meta = MetaHypo(prior)
     for taxon in sample:
         meta.Update(taxon)
 
+    curves = []
+    iters = 50
+    for i in range(iters):
+        taxons, curve = meta.GenerateRarefactionCurve(sample, 15)
+        print taxons
+        curves.append(curve)
+
+    PlotCurves(curves)
     meta.Print()
-    #meta.PlotHypos('other')
-    return
 
-    MakeCurve('aab')
-    return
-
-    inventory = list('abcdefghijk')
-
-    census = Census(inventory)
-
-    sample = 'aaaaaaaaabbbbb'
-    sample = 'aaaaaabbbbcdef'
-    evidence = Pmf.MakeHistFromList(sample)
-    census.Update(evidence)
-    census.Plot()
-    census.PlotPmfs()
-
-    census.PlotBeforeAndAfter(sample)
 
 if __name__ == '__main__':
     main()
