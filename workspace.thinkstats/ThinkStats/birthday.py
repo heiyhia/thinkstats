2e115baba77adf3eabea366cb8545942ff53e592
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2011-09-01 17:14:31 +0000

    Adding birthday.py

---
+++ workspace.thinkstats/ThinkStats/birthday.py
@@ -0,0 +1,30 @@
+"""This file contains code used in "Think Stats",
+by Allen B. Downey, available from greenteapress.com
+
+Copyright 2010 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+"""
+
+import math
+import random
+import matplotlib.pyplot as pyplot
+import Cdf
+import myplot
+import Pmf
+
+
+cdfs = []
+for i in range(10):
+    n = 30
+    t = [random.randrange(365) for i in range(n)]
+    t.sort()
+
+    pmf = Pmf.Pmf()
+    for i in range(len(t)-1):
+        x = t[i+1] - t[i]
+        pmf.Incr(x)
+
+    cdf = Cdf.MakeCdfFromPmf(pmf)
+    cdfs.append(cdf)
+
+myplot.Cdfs(cdfs, root='birthday', transform='exponential')
