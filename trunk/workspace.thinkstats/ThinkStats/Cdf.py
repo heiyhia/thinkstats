ccf8663424c8dd2733b8b263733ad201f0c6d495
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2011-09-13 15:06:24 +0000

    Adding probless.py

--- trunk/workspace.thinkstats/ThinkStats/Cdf.py
+++ trunk/workspace.thinkstats/ThinkStats/Cdf.py
@@ -119,7 +119,7 @@ class Cdf(object):
             old_p = new_p
         return total
 
-    def Round(self, multiplier=1000.0):
+    def _Round(self, multiplier=1000.0):
         """
         An entry is added to the cdf only if the percentile differs
         from the previous value in a significant digit, where the number
@@ -233,43 +233,3 @@ def MakeCdfFromList(seq, name=''):
     return MakeCdfFromHist(hist, name)
 
 
-def ProbLess(cdf1, cdf2):
-    """Probability that a value from cdf1 is less than one from cdf2.
-    For continuous distributions F and G, the chance that a sample
-    from F is less than a sample from G is
-    Integral(x): pdf_F(x) * (1 - cdf_G(x))
-    This function computes an approximation of this Integral using
-    discrete CDFs.
-
-    Args:
-        cdf1: CDF object
-        cdf2: CDF object
-
-    Returns:
-        float probability
-    """
-    total = 0.0
-    i = 0
-    j = 0
-    x = float('-Inf')
-    while True:
-        # sweep through cdf1 and compute p, the marginal prob of v2
-        unused_x1, p1 = cdf1.data[i]
-        x2, p2 = cdf1.data[i+1]
-        p = p2 - p1
-    
-        # incr through cdf2 to find Prob{x < x2}
-        while x <= x2:
-            x, y = cdf2.data[j]
-            if j == len(cdf2.data)-1:
-                break
-            else:
-                j += 1
-    
-        # add up the integral
-        total += p * (1 - y)
-        i += 1
-        if i == len(cdf1.data)-1:
-            break
-
-    return total
