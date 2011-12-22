0b01ab1459318bc726b930746214b16e64e1fc3c
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2011-12-22 20:35:25 +0000

    Working on erf.c

--- workspace.thinkstats/ThinkStats/erf.py
+++ workspace.thinkstats/ThinkStats/erf.py
@@ -1,20 +1,23 @@
 """This file contains code for use with "Think Stats",
 by Allen B. Downey, available from greenteapress.com
 
-Copyright 2010 Allen B. Downey
+Copyright 2011 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
-Credits:
-
-erf is a modified version of a function contributed to the public
-domain by John D. Cook
-http://www.johndcook.com/python_erf.html
 """
 
+import numpy
 import math
 from scipy.special import erf, erfinv
 
+import Cdf
+import Pmf
+
 root2 = math.sqrt(2.0)
+
+
+def StandardNormalCdf(x):
+    return (erf(x / root2) + 1) / 2
 
 
 def NormalCdf(x, mu=0, sigma=1):
@@ -30,9 +33,7 @@ def NormalCdf(x, mu=0, sigma=1):
     Returns:
         float
     """
-    xx = (x - mu) / sigma
-    y = (erf(xx / root2) + 1) / 2
-    return y
+    return StandardNormalCdf((x - mu) / sigma)
 
 
 def NormalCdfInverse(p, mu=0, sigma=1):
@@ -50,4 +51,60 @@ def NormalCdfInverse(p, mu=0, sigma=1):
     """
     x = root2 * erfinv(2*p - 1)
     return mu + x * sigma
+
+
+spread = 4.0
+
+def MakeNormalCdf(low=-spread, high=spread, digits=2):
+    """Returns a Cdf object with the standard normal CDF.
+
+    low: how many standard deviations below the mean?
+    high: how many standard deviations above the mean?
+    digits:
+    """
+    n = (high - low) * 10**digits + 1
+    xs = numpy.linspace(low, high, n)
+    ps = (erf(xs / root2) + 1) / 2
+    cdf = Cdf.Cdf(xs, ps)
+    return cdf
+
+
+def MakeNormalPmf(low=-spread, high=spread, digits=2):
+    """Returns a Pmf object with the standard normal CDF.
+
+    low: how many standard deviations below the mean?
+    high: how many standard deviations above the mean?
+    digits:
+    """
+    cdf = MakeNormalCdf(low, high, digits)
+    pmf = Pmf.MakePmfFromCdf(cdf)
+    return pmf
+
+
+class FixedPointNormalPmf(Pmf.Pmf):
+
+    def __init__(self, spread=4, digits=2):
+        Pmf.Pmf.__init__(self)
+        self.spread = spread
+        self.digits = digits
+
+        n = 2 * spread * 10**digits + 1
+        xs = numpy.linspace(-spread, spread, n)
+        gap = (xs[1] - xs[0]) / 2
+
+        for x in xs:
+            p = StandardNormalCdf(x + gap) - StandardNormalCdf(x - gap)
+            self.Set(x, p)
+
+        self.default = p
+        max_like = self.MaxLike()
+
+        self.Normalize(1 / max_like)
+
+    def Round(self, x):
+        return round(x, self.digits)
+    
+    def Probber(self):
+        return lambda x: self.d.get(round(x, self.digits), self.default)
+
 
--- workspace.thinkstats/ThinkStats/erf_test.py
+++ workspace.thinkstats/ThinkStats/erf_test.py
@@ -6,7 +6,9 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
 import unittest
+
 import erf
+import myplot
 
 class Test(unittest.TestCase):
 
@@ -29,6 +31,28 @@ class Test(unittest.TestCase):
 
         x = erf.erfinv(-0.11246296562219549)
         self.assertAlmostEquals(x, -0.1)
+
+    def testMakeNormalCdf(self):
+        cdf = erf.MakeNormalCdf(digits=2)
+        #self.assertAlmostEquals(cdf.Prob(-3.0), 0.0013498980316301035)
+        #self.assertAlmostEquals(cdf.Prob(0.0), 0.5)
+        #self.assertAlmostEquals(cdf.Prob(3.0), 0.9986501019683699)
+        #self.assertAlmostEquals(cdf.Prob(5.0), 0.99996832875816688)
+
+    def testMakeNormalPmf(self):
+        pmf = erf.MakeNormalPmf(digits=1)
+
+        #self.assertAlmostEquals(pmf.Prob(-3.0), 3.503234174478953e-05)
+        #self.assertAlmostEquals(pmf.Prob(0.0), 0.0031915042004636573)
+        #self.assertAlmostEquals(pmf.Prob(3.0), 0.9986501019683699)
+        
+    def testFixedPointNormalPmf(self):
+        pmf = erf.FixedPointNormalPmf(spread=3, digits=1)
+        
+        probber = pmf.Probber()
+        for x in [-1.01, 1.01]:
+            print x, probber(x)
+        #myplot.Hist(pmf, show=True)
 
 
 if __name__ == "__main__":
