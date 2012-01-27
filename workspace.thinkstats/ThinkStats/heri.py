1c9619a07888b941f8b5e9b13b9bf4bd19d1f786
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2012-01-27 21:54:26 +0000

    Adding heri.2

--- workspace.thinkstats/ThinkStats/heri.py
+++ workspace.thinkstats/ThinkStats/heri.py
@@ -19,6 +19,161 @@ import rpy2.robjects as robjects
 r = robjects.r
 
 
+def Regress(model, ys, ts, print_flag=False, drop=1):
+    """Run a linear regression using rpy2.
+
+    Returns a list of coefficients (which are rpy2.RVectors)
+    Use GetEst to extract the estimated coefficient from a coeff.
+    """
+    t2 = [t**2 for t in ts]
+
+    # put the data into the R environment
+    robjects.globalEnv['ts'] = robjects.FloatVector(ts[:-drop])
+    robjects.globalEnv['t2'] = robjects.FloatVector(t2[:-drop])
+    robjects.globalEnv['ys'] = robjects.FloatVector(ys[:-drop])
+
+    model = r(model)
+    res = r.lm(model)
+    if print_flag:
+        PrintSummary(res)
+
+    coeffs = GetCoefficients(res)
+    return coeffs
+
+
+def GetEst(coeff):
+    """Extracts the estimated coefficient from a coeff."""
+    name, est, stderr = coeff
+    return est
+
+
+def GetCoefficients(res):
+    """Extracts coefficients from r.lm.
+
+    This is an awful function.  It actually generates a text representation
+    of the results and then parses it.  Ack!
+
+    Maybe the rpy2 interface (or it's documentation) will improve at
+    some point so this nonsense is no longer necessary.
+    """
+    flag = False
+    lines = r.summary(res)
+    lines = str(lines)
+
+    coeffs = []
+    for line in lines.split('\n'):
+        line = line.strip()
+        if flag:
+            t = line.split()
+            if len(t) < 5:
+                break
+            name, est, stderr = t[0], float(t[1]), float(t[2])
+            coeffs.append((name, est, stderr))
+
+        # skip everything until we get to the coefficients
+        if line.startswith('Estimate'):
+            flag = True
+
+    return coeffs
+
+
+def MakeFit(model, ys, ts):
+    """Fit a model to the data and return the fitted values."""
+    coeffs = Regress(model, ys, ts)
+    fys = EvalFit(coeffs, ts)
+    return fys
+
+
+def Residuals(model, ys, ts):
+    """Fit a model to the data and return the residuals."""
+    coeffs = Regress(model, ys, ts, print_flag=True)
+    fys = EvalFit(coeffs, ts)
+    residuals = [fy - y for fy, y in zip(fys, ys)]
+    return residuals
+
+
+def EvalFit(coeffs, ts):
+    """Evaluate a fitted model at a sequence of locations.
+
+    coeffs: a list of coefficients as returned by rpy2
+    ts: locations to evaluate the model
+
+    Returns a list of fitted values.
+    """
+    betas = [GetEst(coeff) for coeff in reversed(coeffs)]
+    fys = [Horner(betas, t) for t in ts]
+    return fys
+
+
+def Horner(betas, t):
+    """Use Horner's method to evaluate a polynomial.
+
+    betas: coefficients in decreasing order of power.
+    t: where to evaluate
+    """
+    total = 0
+    for beta in betas:
+        total = total * t + beta
+    return total
+
+
+def MakeErrorModel(model, ys, ts, n=300):
+    """Makes a model that captures sample error and residual error.
+
+    model: string representation of the regression model
+    ys:    dependent variable
+    ts:    explanatory variable
+    n:     number of simulations to run
+
+    Returns a pair of models, where each model is a pair of rows.
+    """
+    # estimate mean and stddev of the residuals
+    residuals = Residuals(model, ys, ts)
+    mu, var = thinkstats.MeanVar(residuals)
+    sig = math.sqrt(var)
+
+    # make the best fit
+    fys = MakeFit(model, ys, ts)
+
+    # resample residuals and generate hypothetical fits
+    fits = []
+    for i in range(n):
+        fake_ys = [fy + random.gauss(mu, sig) for fy in fys]
+        fake_fys = MakeFit(model, fake_ys, ts)
+        fits.append(fake_fys)
+
+    # find the 90% CI in each column
+    columns = zip(*fits)
+
+    sample_error = MakeIntervals(columns)
+    columns = AddResidualError(columns, mu, sig)
+    total_error = MakeIntervals(columns)
+
+    return sample_error, total_error
+
+
+def MakeIntervals(columns, low=5, high=95):
+    """Find a confidence interval for each columns.
+
+    Returns two rows: the low end of the intervals and the high ends.
+    """
+    cdfs = [Cdf.MakeCdfFromList(ys) for ys in columns]
+    min_fys = [cdf.Percentile(low) for cdf in cdfs]
+    max_fys = [cdf.Percentile(high) for cdf in cdfs]
+    return min_fys, max_fys
+
+
+def AddResidualError(columns, mu, sig):
+    """Adds Gaussian noise to the data in columns.
+
+    columns: list of columns, where each column is a set of y-values
+             for a given t-value
+    mu, sig: parameters of the noise
+    """
+    return [[y + random.gauss(mu, sig) for y in col]
+            for col in columns]
+
+
 def ReadData(filename):
     """Reads a CSV file of SAT scores.
 
@@ -41,193 +196,35 @@ def ReadData(filename):
     return res
 
 
-def Pvalue(filename='heri.0', delta=0.033, n=100000):
-    data = ReadData(filename)
-    
-    count = 0
-    for i in range(n):
-        xs, ys = zip(*data)
-        ys = list(ys)
-        random.shuffle(ys)
-        inter, slope = correlation.LeastSquares(xs, ys)
-
-        if abs(slope) > delta:
-            count += 1
-    return float(count) / n
-
-
-def Fit(xs, ys):
-    """Find the linear least squares fit between xs and ys."""
-    inter, slope = correlation.LeastSquares(xs, ys)
-    print '(inter, slope):', inter, slope
-
-    res = correlation.Residuals(xs, ys, inter, slope)
-    R2 = correlation.CoefDetermination(ys, res)
-
-    print 'inter', inter
-    print 'slope', slope
-    print 'R^2', R2
-    print
-
-    return inter, slope, R2
-
-
-def PlotDiffs(filename='heri.0', root='heri1', flag=False):
+def MakePlot(filename, model, ylabel):
+    """Generates a plot with the data, a fitted model, and error bars."""
     pyplot.clf()
 
-    data = ReadData(filename)
-    xs, ys = zip(*data)
-
-    if flag:
-        RunFit(xs, ys)
-    
-    pyplot.plot(xs, ys, 'b.:', markersize=15)
-    myplot.Save(root=root,
-                title='Yearly changes',
-                xlabel='',
-                ylabel='percentage points',
-                axis=[1972, 2013, -1.2, 2.1])
-
-
-def RunFit(xs, ys):
-    inter, slope, R2 = Fit(xs, ys)
-    fxs = [min(xs), max(xs)]
-    fys = [inter + slope*x for x in fxs]
-    pyplot.plot(fxs, fys, 'r-', linewidth=2)
-    print 'Mean diff', thinkstats.Mean(ys)
-    print 'Current rate:', fys[-1]
-
-
-def PlotProbs(filename='p.heri.31'):
-    pyplot.clf()
-    for x in [1975.5, 1984.5, 1998.5, 2006.5]:
-        xs = [x, x]
-        ys = [0, 1]
-        pyplot.plot(xs, ys, color='0.8', linewidth=10)
-
-    data = ReadData(filename)
-    xs, ys = zip(*data)
-    pyplot.plot(xs, ys, 'bo-', color='blue', linewidth=2, markersize=6)
-    myplot.Save(root='heri2',
-                title='Location of changepoints',
-                xlabel='',
-                ylabel='cumulative probability',
-                axis=[1972, 2010, 0, 1])
-
-
-def RunModel(model, print_flag=False):
-    """Submits model to r.lm and returns the result."""
-    model = r(model)
-    res = r.lm(model)
-    if print_flag:
-        PrintSummary(res)
-    return res
-
-
-def Regress(model, ys, ts, print_flag=False, drop=1):
-    t2 = [t**2 for t in ts]
-
-    # put the data into the R environment
-    robjects.globalEnv['ts'] = robjects.FloatVector(ts[:-drop])
-    robjects.globalEnv['t2'] = robjects.FloatVector(t2[:-drop])
-    robjects.globalEnv['ys'] = robjects.FloatVector(ys[:-drop])
-
-    res = RunModel(model, print_flag)
-    coeffs = GetCoefficients(res)
-    return coeffs
-
-
-def EvalFit(coeffs, ts):
-    betas = [GetEst(coeff) for coeff in reversed(coeffs)]
-    fys = [Horner(betas, t) for t in ts]
-    return fys
-
-
-def MakeFit(model, ys, ts):
-    coeffs = Regress(model, ys, ts)
-    fys = EvalFit(coeffs, ts)
-    return fys
-
-
-def Residuals(model, ys, ts):
-    coeffs = Regress(model, ys, ts, print_flag=True)
-    fys = EvalFit(coeffs, ts)
-    residuals = [fy - y for fy, y in zip(fys, ys)]
-    return residuals
-
-
-def Horner(betas, t):
-    total = 0
-    for beta in betas:
-        total = total * t + beta
-    return total
-
-
-def MakeSampleError(model, ys, ts, n=100):
-    # make a model of the residuals
-    residuals = Residuals(model, ys, ts)
-    mu, var = thinkstats.MeanVar(residuals)
-    sig = math.sqrt(var)
-
-    # make the best fit
-    fys = MakeFit(model, ys, ts)
-
-    # resample residuals and generate fake fits
-    fits = []
-    for i in range(n):
-        fake_ys = [fy + random.gauss(mu, sig) for fy in fys]
-        fake_fys = MakeFit(model, fake_ys, ts)
-        fits.append(fake_fys)
-
-    # plot the 90% CI in each column
-    columns = zip(*fits)
-    AddResidualError(columns, mu, sig)
-
-    cdfs = [Cdf.MakeCdfFromList(ys) for ys in columns]
-    max_fys = [cdf.Percentile(95) for cdf in cdfs]
-    min_fys = [cdf.Percentile(5) for cdf in cdfs]
-
-    return min_fys, max_fys
-
-
-def AddResidualError(columns, mu, sig):
-    for col in columns:
-        for i in range(len(col)):
-            col[i] += random.gauss(mu, sig)
-
-def PlotData(ys, ts):
-    pyplot.plot(ts, ys, 'bo-', linewidth=2, markersize=10)
-
-
-def GetData(filename):
+    # do the analysis without the last data point
     data = ReadData(filename)
     ts, ys = zip(*data)
-    return ts, ys
 
-
-def MakePlot(filename='heri.1'):
-    # do the analysis without the last data point
-    ts, ys = GetData(filename)
+    # shift the times to start at 0 (but use the originals for plots)
     shift = ts[0]
     tshift = [t-shift for t in ts]
 
-    # plot sample error cone
-    model = 'ys ~ ts + t2'
-    min_fys, max_fys = MakeSampleError(model, ys, tshift)
-    pyplot.fill_between(ts, min_fys, max_fys, color='0.9')
+    # plot the error models
+    sample_error, total_error = MakeErrorModel(model, ys, tshift)
+    pyplot.fill_between(ts, *total_error, color='0.9')
+    pyplot.fill_between(ts, *sample_error, color='0.7')
 
-    # plot fit
+    # plot the estimated fit
     fys = MakeFit(model, ys, tshift)
     pyplot.plot(ts, fys, color='red', linewidth=2)
 
-    # plot data (including the last data point
-    PlotData(ys, ts)
+    # plot the data
+    pyplot.plot(ts, ys, 'bo-', linewidth=2, markersize=10)
 
-    myplot.Save(root='heri4',
+    myplot.Save(root=filename,
                 title='',
                 xlabel='',
-                ylabel='Percent None',
-                axis=[1968, 2013, 0, 27])
+                ylabel=ylabel,
+                axis=[1967, 2013, 0, 28])
 
 
 def PlotResiduals(ts, ys):
@@ -239,12 +236,6 @@ def PlotResiduals(ts, ys):
                 xlabel='',
                 ylabel='Residuals',
                 axis=[1968, 2012, -6, 6])
-
-
-
-def GetEst(coeff):
-    name, est, stderr = coeff
-    return est
 
 
 def PrintSummary(res):
@@ -262,32 +253,9 @@ def PrintSummary(res):
     print
 
 
-def GetCoefficients(res):
-    """Prints results from r.lm (just the parts we want)."""
-    flag = False
-    lines = r.summary(res)
-    lines = str(lines)
-
-    coeffs = []
-    for line in lines.split('\n'):
-        line = line.strip()
-        if flag:
-            t = line.split()
-            if len(t) < 5:
-                break
-            name, est, stderr = t[0], float(t[1]), float(t[2])
-            coeffs.append((name, est, stderr))
-
-        # skip everything until we get to the coefficients
-        if line.startswith('Estimate'):
-            flag = True
-
-    return coeffs
-
-
 def main(script):
-
-    MakePlot()
+    MakePlot(filename='heri.1', model='ys ~ ts + t2', ylabel='Religion None')
+    MakePlot(filename='heri.2', model='ys ~ ts + t2', ylabel='Attendance None')
 
 
 if __name__ == '__main__':
