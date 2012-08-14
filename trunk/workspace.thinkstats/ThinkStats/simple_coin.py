8971bdc0fb24b6da7e63121398dc6d76b0634c75
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2012-08-14 20:21:45 +0000

    Adding simple_coin.py

---
+++ trunk/workspace.thinkstats/ThinkStats/simple_coin.py
@@ -0,0 +1,111 @@
+"""This file contains code used in "Think Stats",
+by Allen B. Downey, available from greenteapress.com
+
+Copyright 2012 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+"""
+
+import myplot
+import Pmf
+from math import pow
+
+"""This file contains a partial solution to a problem from
+MacKay, "Information Theory, Inference, and Learning Algorithms."
+
+    Exercise 3.15 (page 50): A statistical statement appeared in
+    "The Guardian" on Friday January 4, 2002:
+
+        When spun on edge 250 times, a Belgian one-euro coin came
+        up heads 140 times and tails 110.  'It looks very suspicious
+        to me,' said Barry Blight, a statistics lecturer at the London
+        School of Economics.  'If the coin weere unbiased, the chance of
+        getting a result as extreme as that would be less than 7%.'
+
+MacKay asks, "But do these data give evidence that the coin is biased
+rather than fair?"
+
+We will get to that question, but we will start by defining a distribution
+of possible values for p, the probability of heads, and computing the
+posterior distribution of p given the evidence cited.
+
+The code below uses a Pmf object to represent a suite of hypotheses.
+In this case, the values in the Pmf are possible values of p, but in
+general we could use any kind of object to represent a hypothesis.
+
+"""
+
+def Update(pmf, outcome):
+    for x in pmf.Values():
+        if outcome == 'H':
+            pmf.Mult(x, x)
+        else:
+            pmf.Mult(x, 100-x)
+    pmf.Normalize()
+
+
+def MLE(pmf):
+    prob, val = max((prob, val) for val, prob in pmf.Items())
+    return val
+
+
+def Mean(pmf):
+    total = 0
+    for val, prob in pmf.Items():
+        total += val * prob
+    return total
+
+def Percentile(pmf, p):
+    total = 0
+    for val, prob in pmf.Items():
+        total += prob
+        if total >= p:
+            return val    
+
+def TrianglePrior():
+    pmf = Pmf.Pmf()
+    for x in range(0, 50):
+        pmf.Set(x, x)
+    for x in range(51, 101):
+        pmf.Set(x, 100-x) 
+    pmf.Normalize()
+    return pmf
+
+def Main():
+    pmf = Pmf.Pmf()
+    for x in range(0, 101):
+        pmf.Set(x, 1)
+    pmf.Normalize()
+
+    pmf = TrianglePrior()
+    # plot the posterior distributions
+    myplot.Pmf(pmf,
+               root='simple_coin_triangle_prior',
+               title='Biased coin',
+               xlabel='x',
+               ylabel='Probability')
+
+
+    evidence = 'H' * 140 + 'T' * 110
+
+    for outcome in evidence:
+        Update(pmf, outcome)
+
+    print pmf.Prob(50)
+    # 0.0209765261295
+
+    print 'MLE', MLE(pmf)
+
+    print 'Mean', Mean(pmf)
+
+    print '5th %ile', Percentile(pmf, 0.05) 
+    print '95th %ile', Percentile(pmf, 0.95) 
+
+    # plot the posterior distributions
+    myplot.Pmf(pmf,
+               root='simple_coin_triangle_post',
+               title='Biased coin',
+               xlabel='x',
+               ylabel='Probability')
+
+if __name__ == '__main__':
+    Main()
