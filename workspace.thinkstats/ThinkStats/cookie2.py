0b1afcc56697ab0e6ec997c9b535fade2a83c8d3
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2012-09-12 20:43:26 +0000

    Adding cookie problem

---
+++ workspace.thinkstats/ThinkStats/cookie2.py
@@ -0,0 +1,51 @@
+"""This file contains code for use with "Think Bayes",
+by Allen B. Downey, available from greenteapress.com
+
+Copyright 2012 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+"""
+
+from thinkbayes import Pmf
+
+
+class Cookie(Pmf):
+    def __init__(self, hypos):
+        Pmf.__init__(self)
+        for hypo in hypos:
+            self.Set(hypo, 1)
+        self.Normalize()
+
+    def Update(self, data):
+        for hypo in self.Values():
+            like = self.Likelihood(hypo, data)
+            self.Mult(hypo, like)
+        self.Normalize()
+
+    mixes = {
+        'Bowl 1':dict(vanilla=0.75, chocolate=0.25),
+        'Bowl 2':dict(vanilla=0.5, chocolate=0.5),
+        }
+
+    def Likelihood(self, hypo, data):
+        mix = self.mixes[hypo]
+        like = mix[data]
+        return like
+
+
+def main():
+    hypos = ['Bowl 1', 'Bowl 2']
+    dataset = ['vanilla', 'vanilla', 'vanilla']
+
+    pmf = Cookie(hypos)
+
+    pmf.Update('vanilla')
+
+    for data in dataset:
+        pmf.Update(data)
+    
+    for hypo, prob in pmf.Items():
+        print hypo, prob
+
+
+if __name__ == '__main__':
+    main()
