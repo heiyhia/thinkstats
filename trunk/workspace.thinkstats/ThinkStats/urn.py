298bf8f7833c4ae2b9754a10acd27150420a7aad
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2012-09-17 21:06:42 +0000

    Adding urn.py

---
+++ trunk/workspace.thinkstats/ThinkStats/urn.py
@@ -0,0 +1,44 @@
+"""This file contains code for use with "Think Bayes",
+by Allen B. Downey, available from greenteapress.com
+
+Copyright 2012 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+"""
+
+import myplot
+
+from thinkbayes import Suite
+
+
+class Urn(Suite):
+    def Likelihood(self, hypo, data):
+        """Computes the likelihood of the data under the hypothesis.
+
+        hypo: integer number of blue marbles (out of three)
+        data: string 'B' or 'G'
+        """
+        p = hypo / 3.0
+        if data == 'B':
+            return p
+        else:
+            return 1-p
+  
+
+def main():
+    suite = Urn([0,1,2,3])
+
+    for data in 'BBBBB':
+        suite.Update(data)
+
+    suite.Print()
+    print suite.Mean() / 3.0
+
+    for data in 'B'*12 + 'G'*2:
+        suite.Update(data)
+
+    suite.Print()
+    print suite.Mean() / 3.0
+
+
+if __name__ == '__main__':
+    main()
