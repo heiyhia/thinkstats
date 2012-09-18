9a186205e85534ec4b39e5b1f8e98f72a1da3360
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2012-09-18 19:36:11 +0000

    Working on euro.py

--- trunk/workspace.thinkstats/ThinkStats/train.py
+++ trunk/workspace.thinkstats/ThinkStats/train.py
@@ -8,20 +8,10 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 from dice import Dice
 import myplot
 
+
 class Train(Dice):
     """The likelihood function for the train problem is the same as
     for the Dice problem."""
-
-
-def Mean(suite):
-    """Computes the mean of a Pmf.
-
-    This is just an example; it would be better to use Pmf.Mean()
-    """
-    total = 0
-    for hypo, prob in suite.Items():
-        total += hypo * prob
-    return total
 
 
 def main():
@@ -29,15 +19,13 @@ def main():
     suite = Train(hypos)
 
     suite.Update(60)
+    print suite.Mean()
 
     myplot.Pmf(suite)
     myplot.Save(root='train1',
                 xlabel='Number of trains',
                 ylabel='Probability',
                 formats=['pdf', 'eps'])
-
-    print Mean(suite)
-    print suite.Mean()
 
 
 if __name__ == '__main__':
