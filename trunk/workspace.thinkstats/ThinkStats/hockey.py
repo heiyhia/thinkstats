7ce6f6c909bdbfe2504bddae90dba5fbbef2d9f9
allendowney <allendowney@35162612-2e27-ec42-9b7d-d9f005073bc0>
2012-09-27 14:11:54 +0000

    Working on hockey.py

--- trunk/workspace.thinkstats/ThinkStats/hockey.py
+++ trunk/workspace.thinkstats/ThinkStats/hockey.py
@@ -8,11 +8,11 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 import math
 import numpy
 
+import thinkbayes
 import myplot
-from thinkbayes import Suite
 
 
-def norm_pdf(x, mu, sigma):
+def EvalGaussianPdf(x, mu, sigma):
     """Computes the unnormalized PDF of the normal distribution.
 
     x: value
@@ -26,11 +26,69 @@ def norm_pdf(x, mu, sigma):
     return p
 
 
-class Hockey(Suite):
+def EvalPoissonPmf(lam, t, k):
+    """Computes the Poisson PMF.
+
+    lam: parameter lambda in events per unit time
+    t: duration in units of time
+    k: number of events
+
+    returns: float probability
+    """
+    return (lam*t)**k * math.exp(lam*t) / math.factorial(k)
+
+
+def MakePoissonPmf(lam, t, high):
+    """Makes a PMF discrete approx to a Poisson distribution.
+
+    lam: parameter lambda in events per unit time
+    t: duration in units of time
+    high: upper bound of the Pmf
+
+    returns: normalized Pmf
+    """
+    pmf = thinkbayes.Pmf()
+    for k in range(0, high+1):
+        p = EvalPoissonPmf(lam, t, k)
+        pmf.Set(k, p)
+    pmf.Normalize()
+    return pmf
+
+
+def EvalExponentialPdf(lam, x):
+    """Computes the exponential PDF.
+
+    lam: parameter lambda in events per unit time
+    x: value
+
+    returns: float probability density
+    """
+    return lam * exp(-lam * x)
+
+
+def MakeExponentialPmf(lam, high, n=200):
+    """Makes a PMF discrete approx to an exponential distribution.
+
+    lam: parameter lambda in events per unit time
+    high: upper bound
+    n: number of values in the Pmf
+
+    returns: normalized Pmf
+    """
+    pmf = thinkbayes.Pmf()
+    for x in numpy.arange(0, high, n):
+        p = EvalExponentialPmf(lam, x)
+        pmf.Set(x, p)
+    pmf.Normalize()
+    return pmf
+
+
+class Hockey(thinkbayes.Suite):
     def __init__(self):
-        Suite.__init__(self)
-        for x in numpy.arange(1.5, 3.9, 0.05):
-            p = norm_pdf(x, 2.7, 0.3)
+        thinkbayes.Suite.__init__(self)
+
+        for x in numpy.arange(1.5, 4.9, 0.05):
+            p = EvalGaussianPdf(x, 2.7, 0.3)
             self.Set(x, p)
             
     def Likelihood(self, hypo, data):
@@ -44,26 +102,52 @@ class Hockey(Suite):
         hypo: goal scoring rate in goals per game
         data: goals scored in one period
         """
-        t = 1.0 / 3.0
+        t = 1.0
         lam = hypo
         k = data
         like = (lam*t)**k * math.exp(lam*t)
         return like
 
 
+def MakeGoalPmf(suite):
+    """Makes the distribution of goals scored, given distribution of lam.
+
+    suite: distribution of goal-scoring rate
+
+    returns: Pmf of goals per game
+    """
+    pmfs = thinkbayes.Pmf()
+    t = 1.0
+    high = 10
+
+    for lam, prob in suite.Items():
+        pmf = MakePoissonPmf(lam, t, high)
+        pmfs.Set(pmf, prob)
+
+    mix = thinkbayes.MakeMixture(pmfs, name=suite.name)
+    return mix
+
+
 def main():
     suite1 = Hockey()
     suite1.name = 'bruins'
-    suite1.Update(1)
-    myplot.Pmf(suite1)
+    suite1.UpdateSet([5, 3, 1])
+    mix1 = MakeGoalPmf(suite1)
+    myplot.Pmf(mix1)
     
     suite2 = Hockey()
     suite2.name = 'sabres'
-    suite2.Update(0)
-    myplot.Pmf(suite2)
+    suite2.UpdateSet([1, 2, 3])
+    mix2 = MakeGoalPmf(suite2)
+    myplot.Pmf(mix2)
     
     myplot.Show()
 
+    p_win = thinkbayes.PmfProbGreater(suite1, suite2)
+    p_loss = thinkbayes.PmfProbLess(suite1, suite2)
+    p_tie = thinkbayes.PmfProbEqual(suite1, suite2)
+
+    print p_win, p_loss, p_tie
 
 if __name__ == '__main__':
     main()
